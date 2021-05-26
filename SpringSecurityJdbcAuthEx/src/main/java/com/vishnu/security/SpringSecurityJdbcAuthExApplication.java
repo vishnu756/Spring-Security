27cf9f54811d40ddfad232b596f3fbb5c10aa6ac
vishnu756 <60641444+vishnu756@users.noreply.github.com>
2021-05-26 11:54:25 +0530

    FIRSRT COMMIT

---
+++ SpringSecurityJdbcAuthEx/src/main/java/com/vishnu/security/SpringSecurityJdbcAuthExApplication.java
@@ -0,0 +1,13 @@
+package com.vishnu.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityJdbcAuthExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityJdbcAuthExApplication.class, args);
+	}
+
+}
---
+++ SpringSecurityJdbcAuthEx/src/test/java/com/vishnu/security/SpringSecurityJdbcAuthExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vishnu.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityJdbcAuthExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
